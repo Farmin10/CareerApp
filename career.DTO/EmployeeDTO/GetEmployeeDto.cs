74598b3555ccb856969749775de106b85fd3b9dd
Farmin10 <farmin.rustamov@mail.ru>
2021-07-08 10:06:58 +0300

    responces refactored

--- career.DTO/EmployeeDTO/GetEmployeeDto.cs
+++ career.DTO/EmployeeDTO/GetEmployeeDto.cs
@@ -16,6 +16,7 @@ namespace career.DTO.EmployeeDTO
         public string PhoneNumber { get; set; }
         public string Email { get; set; }
         public string Pin { get; set; }
+        public string  PicturePath { get; set; }
         public GetDepartmantForEmployeeDto Departmant { get; set; }
         public GetPositionForEmployeeDto Position { get; set; }
     }
